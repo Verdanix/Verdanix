a5b5330720464910607b9cda29d6a500a529698d
verdanix <contact@masonroot.dev>
2024-12-15 19:07:10 -0800

    Added formatter and linter

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
-    return (
-        <div>
-            <h1>Hello, world!</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+    </div>
+  );
 }
