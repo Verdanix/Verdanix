aa08e419ce734d1e95401579574f9da924940ad3
Mason <contact@masonroot.dev>
2024-12-20 04:00:35 +0000

    Finished responsive header
    - Added XS, SM, MD, and LG sizes

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,6 +1,10 @@
-import './Home.scss';
+
+import Navbar from '../../components/Navbar.jsx';
+
 export default function Home() {
     return (
-        <>Home</>
+        <>
+            <Navbar activeName={'home'} />
+        </>
     )
 }
