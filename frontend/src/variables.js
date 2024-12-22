9b784ca5075d580554c7e28cfe21e1d495d62e06
Mason <contact@masonroot.dev>
2024-12-22 19:57:26 +0000

    Fixed resizing bug.
    - Added padding variable to variable.js
    
    - Added padding to the base style.scss
    
    - Changed the width from <= to < xsm

--- frontend/src/variables.js
+++ frontend/src/variables.js
@@ -1,6 +1,7 @@
-const xsm = 400;
-const sm = 576;
-const md = 768;
-const lg = 992;
+const padding = 16 * 2 - 1;
+const xsm = 400 - padding;
+const sm = 576 - padding;
+const md = 768 - padding;
+const lg = 992 - padding;
 
 export { xsm, sm, md, lg };
