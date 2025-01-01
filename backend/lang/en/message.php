147e0562d0d8b109b0eff6d28d99bd9210459dd9
Mason Root <contact@masonroot.dev>
2025-01-01 09:30:42 -0800

    Added English Localization
    Added
    - English localization for the home page
    - English localization for common use cases

---
+++ backend/lang/en/message.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    // Common
+    'hello_world' => 'Hello World!',
+    'login' => 'Login',
+    'register' => 'Register',
+    'email' => 'Email',
+    'password' => 'Password',
+    'first_name' => 'First Name',
+    'last_name' => 'Last Name',
+
+    // Home page
+    'home_quote_line1' => 'Future-focused.',
+    'home_quote_line2' => 'Innovation fueled.',
+    'home_quote_line3' => 'Built to',
+    'home_quote_line3_end_word' => 'disrupt.',
+    'home_description' => 'I\'m Mason Root, a full-stack software engineer with 5 years of experience.
+                My work quality is consistently exceptional as I strive to achieve the impossible.
+                Working with me is an amazing experience due to my strong communication skills, fast development pace, excellent social abilities, and impressive work ethic.',
+
+    'why_me' => 'Why Me?',
+    'why_section_security' => 'Security is paramount in every app I build. I follow strict safety guidelines throughout development. If any issues arise after launch, I\'ll fix them at no extra cost.',
+    'why_section_speed' => 'Speed is crucial – right after security. I push apps to run as quickly as possible. No one enjoys waiting for simple tasks.',
+    'why_section_scalability' => 'I build apps with growth in mind. Share your vision for the future, and I\'ll design solutions that can adapt as your needs expand.',
+];
