0967ebba8d306253f758c07e83b3176fd3bf75b4
Mason Root <contact@masonroot.dev>
2025-01-01 14:49:45 -0800

    Started work on Home page
    Added
    - Background image
    - Navbar
    - Hero image

--- backend/resources/js/Pages/Home.jsx
+++ backend/resources/js/Pages/Home.jsx
@@ -1,4 +1,5 @@
 import Navbar from '@/Components/Navbar.jsx';
+import { useLaravelReactI18n } from 'laravel-react-i18n';
 
 export default function Home({
     meta_title,
@@ -6,9 +7,55 @@ export default function Home({
     meta_keywords,
     meta_twitter_card,
 }) {
+    const { t } = useLaravelReactI18n();
     return (
         <>
-            <Navbar view="home" />
+            <div className="hero">
+                <Navbar view="home" />
+                <div className="text">
+                    <h1>
+                        {t('message.home_quote_line1')}
+                        <br />
+                        {t('message.home_quote_line2')}
+                        <br />
+                        {t('message.home_quote_line3')}
+                        <strong>
+                            {t('message.home_quote_line3_end_word')}
+                        </strong>
+                    </h1>
+
+                    <div className="stats">
+                        <div className="col1">
+                            <p>
+                                <span>0+</span>
+                                <br />
+                                Projects
+                                <br className="nl" /> completed
+                            </p>
+                            <p>
+                                <span>0+</span>
+                                <br />
+                                Pending
+                                <br className="nl" /> projects
+                            </p>
+                        </div>
+                        <div className="col2">
+                            <p>
+                                <span>0+</span>
+                                <br />
+                                Happy
+                                <br className="nl" /> clients
+                            </p>
+                            <p>
+                                <span>0+</span>
+                                <br />
+                                Hours
+                                <br className="nl" /> worked
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
