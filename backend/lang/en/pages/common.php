cb28e14d9418779faed424506496d9b0749ee116
Mason Root <contact@masonroot.dev>
2025-01-01 15:11:53 -0800

    Updated localizations
    Fixed
    - Navbar localization
    - Home localization
    
    Added
    - Localizations for the project stats
    - Navbar localizations

--- backend/lang/en/pages/common.php
+++ backend/lang/en/pages/common.php
@@ -1,3 +1,13 @@
 <?php
 
-return [];
+return [
+    // Components
+    // Navbar
+    'navbar.home' => 'Home',
+    'navbar.aboutme' => 'About Me',
+    'navbar.projects' => 'Projects',
+    'navbar.resume' => 'Resume',
+    'navbar.pricing' => 'Pricing',
+    'navbar.contact' => 'Contact',
+    'navbar.login' => 'Login',
+];
