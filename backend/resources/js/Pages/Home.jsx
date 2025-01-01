8363f8fcb756cbfa6d0bd1a0b9a1200326bc862e
Mason Root <contact@masonroot.dev>
2025-01-01 15:02:10 -0800

    Updated translation library
    Fixed
    - There is only 1 english translation file. "English is English" - probably said by someone on Reddit
    - Moved from laravel-react-i18n to i18next (https://react.i18next.com/) which is theoretically improves performance and structure. It also allows for multiple translation files
    
    Added
    - The i18next packages
    - i18n translations files
    - Translation route
    
    Removed
    - The laravel-react-i18n package

--- backend/resources/js/Pages/Home.jsx
+++ backend/resources/js/Pages/Home.jsx
@@ -1,5 +1,5 @@
 import Navbar from '@/Components/Navbar.jsx';
-import { useLaravelReactI18n } from 'laravel-react-i18n';
+import { useTranslation } from 'react-i18next';
 
 export default function Home({
     meta_title,
@@ -7,21 +7,19 @@ export default function Home({
     meta_keywords,
     meta_twitter_card,
 }) {
-    const { t } = useLaravelReactI18n();
+    const { t } = useTranslation('home');
     return (
         <>
             <div className="hero">
                 <Navbar view="home" />
                 <div className="text">
                     <h1>
-                        {t('message.home_quote_line1')}
+                        {t('hero.quote.line.1')}
                         <br />
-                        {t('message.home_quote_line2')}
+                        {t('hero.quote.line.2')}
                         <br />
-                        {t('message.home_quote_line3')}
-                        <strong>
-                            {t('message.home_quote_line3_end_word')}
-                        </strong>
+                        {t('hero.quote.line.3')}
+                        <strong>{t('hero.quote.line.4.end.word')}</strong>
                     </h1>
 
                     <div className="stats">
