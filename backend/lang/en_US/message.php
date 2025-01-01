191c2b25f5cead627ab98c1a3a74036b7b3669f7
Mason Root <contact@masonroot.dev>
2025-01-01 09:40:25 -0800

    Updated app.blade.php for SEO
    Fixed
    - SEO
    
    Added
    - SEO tags to the app.blade.php
    - Added seo_keywords to the en_US localization

--- backend/lang/en_US/message.php
+++ backend/lang/en_US/message.php
@@ -2,9 +2,8 @@
 
 return [
     // SEO
-    'seo_base_description' => 'A full-stack software engineer and aspiring entrepreneur from the Bay Area with expertise in Java, Python, React, and scalable development.
-                Passionate about innovation, leadership, and making an impact through coding, problem-solving, and community-driven projects. Explore my journey and projects here.',
-
+    'seo_base_description' => 'A full-stack software engineer and aspiring entrepreneur from the Bay Area with expertise in Java, Python, React, and scalable development. Passionate about innovation, leadership, and making an impact through coding, problem-solving, and community-driven projects. Explore my journey and projects here.',
+    'seo_keywords' => 'mason, root, eagle, scout, eagle scout, frontend, backend, developer, software, engineer, tech, java, javascript, react, html, css, python',
     // Common
     'hello_world' => 'Hello World!',
     'login' => 'Login',
@@ -22,9 +21,7 @@ return [
     'home_quote_line2' => 'Innovation fueled.',
     'home_quote_line3' => 'Built to',
     'home_quote_line3_end_word' => 'disrupt.',
-    'home_description' => 'I\'m Mason Root, a full-stack software engineer with 5 years of experience.
-                My work quality is consistently exceptional as I strive to achieve the impossible.
-                Working with me is an amazing experience due to my strong communication skills, fast development pace, excellent social abilities, and impressive work ethic.',
+    'home_description' => 'I\'m Mason Root, a full-stack software engineer with 5 years of experience. My work quality is consistently exceptional as I strive to achieve the impossible. Working with me is an amazing experience due to my strong communication skills, fast development pace, excellent social abilities, and impressive work ethic.',
 
     'why_me' => 'Why Me?',
     'why_section_security' => 'Security is paramount in every app I build. I follow strict safety guidelines throughout development. If any issues arise after launch, I\'ll fix them at no extra cost.',
