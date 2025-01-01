dec410089abaf6f157476993a2b93e3f41d59c97
Mason Root <contact@masonroot.dev>
2025-01-01 09:40:25 -0800

    Updated app.blade.php for SEO
    Fixed
    - SEO
    
    Added
    - SEO tags to the app.blade.php
    - Added seo_keywords to the en_US localization

--- backend/resources/views/app.blade.php
+++ backend/resources/views/app.blade.php
@@ -1,22 +1,38 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+    <!-- SEO -->
+    <!-- Base -->
+    <title>{{ $page['props']['meta_title'] ?? 'Default Title' }}</title>
+    <meta name="description" content="{{ $page['props']['meta_description'] ?? 'Default Description' }}">
+    <meta name="keywords" content="{{ $page['props']['meta_keywords'] ?? 'Default Keywords' }}">
+    <meta name="author" content="{{ $page['props']['meta_author'] ?? 'Mason Root' }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <!-- Open Graph -->
+    <meta name="og:title" content="{{ $page['props']['meta_title'] ?? 'Default Title' }}">
+    <meta name="og:url" content="https://masonroot.dev/">
+    <meta name="og:description" content="{{ $page['props']['meta_description'] ?? 'Default Description' }}">
 
-        <!-- Scripts -->
-        @routes
-        @viteReactRefresh
-        @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
-        @inertiaHead
-    </head>
-    <body class="antialiased">
-        @inertia
-    </body>
+    <!-- Twitter -->
+    <meta name="twitter:card"
+          content="{{ $page['props']['meta_twitter_card'] ?? $page['props']['meta_description'] ?? 'Default Description' }}">
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+        rel="stylesheet" />
+
+    <!-- Scripts -->
+    @routes
+    @viteReactRefresh
+    @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
+    @inertiaHead
+</head>
+<body class="antialiased">
+@inertia
+</body>
 </html>
