79b2e9c77cf0395b278fe2339b2196b8d3cdba43
Mason Root <contact@masonroot.dev>
2025-01-01 14:50:22 -0800

    Add future me comment

--- backend/resources/js/Pages/Home.jsx
+++ backend/resources/js/Pages/Home.jsx
@@ -55,6 +55,7 @@ export default function Home({
                         </div>
                     </div>
                 </div>
+                {/* TODO: ADD OTHER SECTIONS */}
             </div>
         </>
     );
