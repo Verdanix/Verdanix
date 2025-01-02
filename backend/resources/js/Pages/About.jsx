40cf22603ef494061ba8b53a73a2bee497430c48
Mason Root <contact@masonroot.dev>
2025-01-02 00:43:16 -0800

    Updated CSS and Created About home page
    Fixed
    - Updated media queries. For app.scss, the mobile and mobile-small are now grouped
    - Code style for Navbar.jsx
    
    Added
    - /about route to web.php
    - About Me page
    - About me translation

---
+++ backend/resources/js/Pages/About.jsx
@@ -0,0 +1,21 @@
+import Navbar from '@/Components/Navbar.jsx';
+import { useTranslation } from 'react-i18next';
+import '../../css/Pages/About.scss';
+
+export default function About({
+    meta_title,
+    meta_description,
+    meta_keywords,
+    meta_twitter_card,
+}) {
+    const { t } = useTranslation('about');
+    return (
+        <>
+            <Navbar view="about" />
+            <h1>{t('story.title')}</h1>
+            <p>{t('story.description')}</p>
+            <h2>{t('skillset.title')}</h2>
+            <p>{t('skillset.description')}</p>
+        </>
+    );
+}
