476a08095f7723a349c106db13ab03224a5d6212
Mason Root <contact@masonroot.dev>
2025-01-02 03:38:08 -0800

    Created login page
    Fixed
    - Auth and validation localizations
    
    Added
    - Login page
    - Login.scss
    - Login localization
    - Error color to variables
    
    Removed
    - Old login page

--- backend/resources/js/Pages/Auth/Login.jsx
+++ backend/resources/js/Pages/Auth/Login.jsx
@@ -1,100 +1,65 @@
-import Checkbox from '@/Components/Checkbox';
-import InputError from '@/Components/InputError';
-import InputLabel from '@/Components/InputLabel';
-import PrimaryButton from '@/Components/PrimaryButton';
-import TextInput from '@/Components/TextInput';
+import '@/../css/Pages/Auth/Login.scss';
 import GuestLayout from '@/Layouts/GuestLayout';
-import { Head, Link, useForm } from '@inertiajs/react';
+import { router, usePage } from '@inertiajs/react';
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
-export default function Login({ status, canResetPassword }) {
-    const { data, setData, post, processing, errors, reset } = useForm({
-        email: '',
-        password: '',
+export default function Login() {
+    const { t } = useTranslation('login');
+    const { errors } = usePage().props;
+
+    const [values, setValues] = useState({
+        email: null,
+        password: null,
         remember: false,
     });
 
+    const handleChange = (e) => {
+        setValues((values) => ({
+            ...values,
+            [e.target.id]: e.target.value,
+        }));
+    };
+
     const submit = (e) => {
         e.preventDefault();
-
-        post(route('login'), {
-            onFinish: () => reset('password'),
-        });
+        router.post('/login', values);
     };
 
     return (
         <GuestLayout>
-            <Head title="Log in" />
-
-            {status && (
-                <div className="mb-4 text-sm font-medium text-green-600">
-                    {status}
-                </div>
-            )}
-
-            <form onSubmit={submit}>
-                <div>
-                    <InputLabel htmlFor="email" value="Email" />
-
-                    <TextInput
+            <div id="form">
+                <h1>{t('h1')}</h1>
+                <p>{Object.values(errors)[0]}</p>
+                <form onSubmit={submit}>
+                    <input
                         id="email"
-                        type="email"
-                        name="email"
-                        value={data.email}
-                        className="mt-1 block w-full"
-                        autoComplete="username"
-                        isFocused={true}
-                        onChange={(e) => setData('email', e.target.value)}
+                        type="text"
+                        placeholder={t('email')}
+                        value={values.email}
+                        onChange={handleChange}
                     />
-
-                    <InputError message={errors.email} className="mt-2" />
-                </div>
-
-                <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password" />
-
-                    <TextInput
+                    <input
                         id="password"
                         type="password"
-                        name="password"
-                        value={data.password}
-                        className="mt-1 block w-full"
-                        autoComplete="current-password"
-                        onChange={(e) => setData('password', e.target.value)}
+                        placeholder={t('password')}
+                        value={values.password}
+                        onChange={handleChange}
                     />
-
-                    <InputError message={errors.password} className="mt-2" />
-                </div>
-
-                <div className="mt-4 block">
-                    <label className="flex items-center">
-                        <Checkbox
-                            name="remember"
-                            checked={data.remember}
-                            onChange={(e) =>
-                                setData('remember', e.target.checked)
-                            }
-                        />
-                        <span className="ms-2 text-sm text-gray-600">
-                            Remember me
-                        </span>
-                    </label>
-                </div>
-
-                <div className="mt-4 flex items-center justify-end">
-                    {canResetPassword && (
-                        <Link
-                            href={route('password.request')}
-                            className="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                        >
-                            Forgot your password?
-                        </Link>
-                    )}
-
-                    <PrimaryButton className="ms-4" disabled={processing}>
-                        Log in
-                    </PrimaryButton>
-                </div>
-            </form>
+                    <div className="loginOptions">
+                        <div id="rememberMe">
+                            <input
+                                type="checkbox"
+                                id="remember"
+                                onChange={handleChange}
+                            />
+                            <label htmlFor="remember">{t('remember_me')}</label>
+                        </div>
+                        <a href="/password-reset">{t('reset_password')}</a>
+                    </div>
+                    <button type="submit">{t('submit')}</button>
+                </form>
+            </div>
         </GuestLayout>
     );
 }
