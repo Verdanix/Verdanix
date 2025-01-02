382866a8c15b71864dd7435423c1b61651d1e1d9
Mason Root <contact@masonroot.dev>
2025-01-02 04:52:09 -0800

    Updated web routes
    Fixed
    - Guest routing is now grouped
    
    Added
    - Grouped via middleware
    
    I'm just procrastinating on life and this project -_-

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,36 +5,37 @@ use App\Http\Resources\ProjectResource;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+Route::middleware('guest')->group(function () {
+    Route::get('/', function () {
+        return Inertia::render('Home', [
+            'meta_title' => trans('seo.home.title'),
+            'meta_description' => trans('seo.home.meta.description'),
+            'meta_keywords' => trans('seo.home.meta.keywords'),
+            'stats' => [
+                'totalProjects' => ProjectResource::allProjectsCount(),
+                'totalPendingProjects' => ProjectResource::allPendingProjects(),
+                'totalClientProjects' => ProjectResource::allClientProjects(),
+                'totalHoursWorked' => ProjectResource::allHoursWorked(),
+            ],
+        ]);
+    });
 
-Route::get('/', function () {
-    return Inertia::render('Home', [
-        'meta_title' => trans('seo.home.title'),
-        'meta_description' => trans('seo.home.meta.description'),
-        'meta_keywords' => trans('seo.home.meta.keywords'),
-        'stats' => [
-            'totalProjects' => ProjectResource::allProjectsCount(),
-            'totalPendingProjects' => ProjectResource::allPendingProjects(),
-            'totalClientProjects' => ProjectResource::allClientProjects(),
-            'totalHoursWorked' => ProjectResource::allHoursWorked(),
-        ],
-    ]);
-});
+    Route::get('/about', function () {
+        return Inertia::render('About', [
+            'meta_title' => trans('seo.about.title'),
+            'meta_description' => trans('seo.about.meta.description'),
+            'meta_keywords' => trans('seo.about.meta.keywords')
+        ]);
+    });
 
-Route::get('/about', function () {
-    return Inertia::render('About', [
-        'meta_title' => trans('seo.about.title'),
-        'meta_description' => trans('seo.about.meta.description'),
-        'meta_keywords' => trans('seo.about.meta.keywords')
-    ]);
-});
-
-Route::get('/translations/{locale}/{page}', function (string $locale, string $page) {
-    $commonPath = base_path("lang/{$locale}/pages/common.php");
-    $path = base_path("lang/{$locale}/pages/{$page}.php");
-    if (file_exists($path)) {
-        return response()->json(array_merge([], include($path), include($commonPath)));
-    }
-    return response()->json([], 404);
+    Route::get('/translations/{locale}/{page}', function (string $locale, string $page) {
+        $commonPath = base_path("lang/{$locale}/pages/common.php");
+        $path = base_path("lang/{$locale}/pages/{$page}.php");
+        if (file_exists($path)) {
+            return response()->json(array_merge([], include($path), include($commonPath)));
+        }
+        return response()->json([], 404);
+    });
 });
 
 Route::get('/dashboard', function () {
