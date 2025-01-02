90ad9d20e1f4ac18221d59606e0ee10fe58694e3
Mason Root <contact@masonroot.dev>
2025-01-02 04:38:55 -0800

    Added Register page
    Added
    - Register Page
    - Register.scss
    
    Removed
    - Password confirmation in controller

--- backend/resources/js/Pages/Auth/Register.jsx
+++ backend/resources/js/Pages/Auth/Register.jsx
@@ -1,9 +1,71 @@
-import InputError from '@/Components/InputError';
-import InputLabel from '@/Components/InputLabel';
-import PrimaryButton from '@/Components/PrimaryButton';
-import TextInput from '@/Components/TextInput';
+import '@/../css/Pages/Auth/Register.scss';
 import GuestLayout from '@/Layouts/GuestLayout';
-import { Head, Link, useForm } from '@inertiajs/react';
+import { router, usePage } from '@inertiajs/react';
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
+export default function Register() {
+    const { t } = useTranslation('register');
+    const { errors } = usePage().props;
+
+    const [values, setValues] = useState({
+        name: null,
+        email: null,
+        password: null,
+        remember: false,
+    });
+
+    const handleChange = (e) => {
+        setValues((values) => ({
+            ...values,
+            [e.target.id]: e.target.value,
+        }));
+    };
+
+    const submit = (e) => {
+        e.preventDefault();
+        router.post('/register', values);
+    };
+
+    return (
+        <GuestLayout>
+            <div id="form">
+                <h1>{t('h1')}</h1>
+                <p>{Object.values(errors)[0]}</p>
+                <form onSubmit={submit}>
+                    <input
+                        id="name"
+                        type="text"
+                        placeholder={t('name')}
+                        value={values.name}
+                        onChange={handleChange}
+                    />
+
+                    <input
+                        id="email"
+                        type="text"
+                        placeholder={t('email')}
+                        value={values.email}
+                        onChange={handleChange}
+                    />
+                    <input
+                        id="password"
+                        type="password"
+                        placeholder={t('password')}
+                        value={values.password}
+                        onChange={handleChange}
+                    />
+                    <div className="loginOptions">
+                        <a href="/login">{t('login')}</a>
+                    </div>
+                    <button type="submit">{t('submit')}</button>
+                </form>
+            </div>
+        </GuestLayout>
+    );
+}
+
+/*
 
 export default function Register() {
     const { data, setData, post, processing, errors, reset } = useForm({
@@ -118,3 +180,4 @@ export default function Register() {
         </GuestLayout>
     );
 }
+*/
