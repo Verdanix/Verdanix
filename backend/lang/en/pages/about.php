00b7e28bf93a8af64647420d02cad61b71a2de10
Mason Root <contact@masonroot.dev>
2025-01-03 12:59:29 -0800

    Added Awards & certs
    Added
    - Mobile responsiveness
    - Awards & Certs section

--- backend/lang/en/pages/about.php
+++ backend/lang/en/pages/about.php
@@ -5,5 +5,10 @@ return [
     'origin.story.1' => 'Growing up, I faced unique challenges as a child with Tourette\'s and ADHD. Despite these obstacles, I\'ve always been driven by an insatiable curiosity and passion for learning. As a young tinkerer, I spent countless hours taking things apart and rebuilding them, or reverse-engineering gadgets like our family\'s Wii to understand how they worked. This maker mentality only intensified as I grew older.',
     'origin.story.2' => 'When I was around 5-6 years old, I showed an early knack for understanding technology by hacking together a way to connect our family\'s Wii to the internet - a feat that my family members didn\'t quite grasp at the time! As I entered elementary school and later junior high, I faced bullying and constant questioning due to my Tourette\'s. However, this adversity only strengthened my resolve to learn and grow.',
     'origin.story.3' => 'I began programming in earnest when I was around 8-10 years old, starting with Python. As I grew older, I became increasingly fascinated by the world of coding and expanded my skills to include Java, JavaScript, TypeScript, HTML, CSS, and more. By age 14, I had already gained a solid foundation in programming, which has since taken me to new heights as a software engineer.',
-    'origin.story.4' => 'Today, I\'m proud to say that my maker mentality, combined with a lifelong passion for learning, has enabled me to become one of the top young software engineers out there - a testament to the power of dedication and perseverance.'
+    'origin.story.4' => 'Today, I\'m proud to say that my maker mentality, combined with a lifelong passion for learning, has enabled me to become one of the top young software engineers out there - a testament to the power of dedication and perseverance.',
+    'awards.certs.title' => 'Awards & Certifications',
+    'awards.certs.start' => 'Throughout my educational journey, I\'ve earned several notable certifications:',
+    'awards.certs.end' => 'These credentials not only demonstrate my technical expertise but also highlight my commitment to lifelong learning and personal growth.',
+    'awards.certs.2' => 'Eagle Scout (2025)',
+    'awards.certs.1' => 'LPI Linux Essentials Certified (2023)',
 ];
