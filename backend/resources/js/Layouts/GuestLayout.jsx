d84d79ae7c5ed826e1fc6a2589be8f4d7e585a75
Mason Root <contact@masonroot.dev>
2025-01-03 19:38:49 -0800

    Added footer section
    Added
    - Footer section
    - Footer localization

--- backend/resources/js/Layouts/GuestLayout.jsx
+++ backend/resources/js/Layouts/GuestLayout.jsx
@@ -1,11 +1,12 @@
 import Navbar from '@/Components/Navbar.jsx';
+import Footer from '@/Components/Footer.jsx';
 
 export default function GuestLayout({ children, view }) {
     return (
         <>
             <Navbar view={view} />
             {children}
-            {/* TODO: ADD OTHER SECTIONS */}
+            <Footer></Footer>
         </>
     );
 }
