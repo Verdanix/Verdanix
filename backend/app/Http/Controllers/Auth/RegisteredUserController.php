87bb75f927e306af6e2602914f5a4b8487b0f1f9
Mason Root <contact@masonroot.dev>
2025-01-05 15:45:23 -0800

    Added Registration page and updated controller
    Fixed
    - Dashboard SFTP upload error
    
    Added
    - Registration page
    - RegisterRequest model for the backend
    
    Removed
    - AuthenticatedLayout from Dashboard. Just because SSH build errors
    -
    -

--- backend/app/Http/Controllers/Auth/RegisteredUserController.php
+++ backend/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,18 +3,35 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\RegisterRequest;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Rules;
+use Illuminate\Validation\ValidationException;
 use Inertia\Inertia;
 use Inertia\Response;
 
 class RegisteredUserController extends Controller
 {
+    /**
+     * Handle an incoming registration request.
+     *
+     * @throws ValidationException
+     */
+    public function store(RegisterRequest $request): RedirectResponse
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        event(new Registered($user));
+
+        return redirect("/login?unverified=1");
+    }
+
     /**
      * Display the registration view.
      */
@@ -25,31 +42,5 @@ class RegisteredUserController extends Controller
             'meta_description' => trans('seo.register.meta.description'),
             'meta_keywords' => trans('seo.register.meta.keywords')
         ]);
-    }
-
-    /**
-     * Handle an incoming registration request.
-     *
-     * @throws \Illuminate\Validation\ValidationException
-     */
-    public function store(Request $request): RedirectResponse
-    {
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|lowercase|email|max:255|unique:'.User::class,
-            'password' => ['required', Rules\Password::defaults()],
-        ]);
-
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
-
-        event(new Registered($user));
-
-        Auth::login($user);
-
-        return redirect(route('dashboard', absolute: false));
     }
 }
