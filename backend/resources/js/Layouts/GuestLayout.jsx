1b05f47a893c0ba81727a20bac7f0488375d80ac
Mason Root <contact@masonroot.dev>
2025-01-05 19:42:35 -0800

    Stole a star animation
    Added
    - A falling star animation
    
    Credit where credit is due. A majority of this code is from alphardex's codepen.
    
    Code: https://codepen.io/alphardex/pen/RwrVoeL
    Github: https://github.com/alphardex
    
    With that said, I did make some changes to the code to make it fit my needs. Such as color and the angle of where the stars fall from. I did __***NOT***__ create the CSS animation

--- backend/resources/js/Layouts/GuestLayout.jsx
+++ backend/resources/js/Layouts/GuestLayout.jsx
@@ -1,9 +1,15 @@
+import { StarAnimation } from '@/Components/Animation.jsx';
 import Footer from '@/Components/Footer.jsx';
 import Navbar from '@/Components/Navbar.jsx';
 
-export default function GuestLayout({ children, view }) {
+export default function GuestLayout({
+    children,
+    view,
+    animation = <StarAnimation />,
+}) {
     return (
         <>
+            {animation}
             <Navbar view={view} />
             {children}
             <Footer></Footer>
