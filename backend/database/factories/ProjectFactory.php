29625ba39e7d5bd6a2f6bbf3dafb972685a1696d
Mason Root <contact@masonroot.dev>
2025-01-08 19:48:50 -0800

    Updated ProjectSeeder
    Added
    - Title column
    
    Removed
    - ProjectsSeeder
    - name column

--- backend/database/factories/ProjectFactory.php
+++ backend/database/factories/ProjectFactory.php
@@ -13,8 +13,10 @@ class ProjectFactory extends Factory
 {
     public function definition(): array
     {
+        $projectTitle = $this->faker->sentence(2);
+        $titleUrl = strtolower(str_replace(' ', '-', $projectTitle));
         return [
-            'name' => $this->faker->name(),
+            'title' => $projectTitle,
             'github_link' => $this->faker->url(),
             'docs_link' => $this->faker->url(),
             'demo_link' => $this->faker->url(),
@@ -22,6 +24,11 @@ class ProjectFactory extends Factory
             'problems_description' => $this->faker->paragraph(2),
             'solutions_description' => $this->faker->paragraph(2),
             'conclusion_description' => $this->faker->paragraph(2),
+            'description' => $this->faker->paragraph(2),
+            'keywords' => $this->faker->words(3, true),
+            'image' => null,
+            'visit_url' => "https://google.com",
+            'more_url' => "https://127.0.0.1:8000/projects/{$titleUrl}/more",
             'hours_worked' => $this->faker->numberBetween(1, 100),
             'is_customer_project' => $this->faker->boolean(),
             'is_pending' => $this->faker->boolean(),
--- backend/routes/auth.php
+++ backend/routes/auth.php
@@ -28,8 +28,6 @@ Route::post('login', [AuthenticatedSessionController::class, 'store']);
      ->name('password.store');*/
 
 Route::middleware('auth')->group(function () {
-
-
     Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
         ->middleware(['signed', 'throttle:6,1'])
         ->name('verification.verify');
