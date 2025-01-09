4894f68edf014d96c1f2a97c7fb98ab3ac6da15f
Mason Root <contact@masonroot.dev>
2025-01-09 05:54:03 -0800

    Added Basic Dashboard
    Added
    - Basic Dashboard with links and order button

--- backend/resources/js/Pages/Dashboard.jsx
+++ backend/resources/js/Pages/Dashboard.jsx
@@ -1,7 +1,14 @@
+import '@/../css/Pages/Dashboard.scss';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.jsx';
+import { useTranslation } from 'react-i18next';
 
 export default function Dashboard() {
+    const { t } = useTranslation('dashboard');
     return (
-        <AuthenticatedLayout view={'manage'}></AuthenticatedLayout>
+        <AuthenticatedLayout view={'manage'}>
+            <div className="content">
+                <a href="/order" className="order">{t('order')}</a>
+            </div>
+        </AuthenticatedLayout>
     );
 }
