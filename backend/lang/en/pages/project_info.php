a9b920e1c885dd8bb6f62a0d37e4c055eead2304
Mason Root <contact@masonroot.dev>
2025-01-08 18:17:38 -0800

    Added Information locale for project Info Page
    Added
    - ProjectInfo localization

---
+++ backend/lang/en/pages/project_info.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'story' => 'Story',
+    'problems' => 'The Problems',
+    'solutions' => 'The Solutions',
+    'conclusion' => 'Conclusion',
+    'back' => 'Back',
+    'github' => 'GitHub',
+    'docs' => 'Docs',
+    'demo' => 'Demo',
+];
