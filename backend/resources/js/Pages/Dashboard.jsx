2440150be6b5623f1534ffcc19f946907d961f84
Mason Root <contact@masonroot.dev>
2025-01-09 05:31:27 -0800

    Added AuthenticatedLayout to Dashboard
    Added
    - AuthenticatedLayout to Dashboard page

--- backend/resources/js/Pages/Dashboard.jsx
+++ backend/resources/js/Pages/Dashboard.jsx
@@ -1,19 +1,7 @@
-import { Head } from '@inertiajs/react';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.jsx';
 
 export default function Dashboard() {
     return (
-        <>
-            <Head title="Dashboard" />
-
-            <div className="py-12">
-                <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                    <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">
-                            You're logged in!
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </>
+        <AuthenticatedLayout view={'manage'}></AuthenticatedLayout>
     );
 }
