060c322f2ba113c6b26c79584dcbff5522b16f7f
Mason Root <contact@masonroot.dev>
2025-03-21 19:51:46 -0700

    Added hero text section locales

--- backend/lang/en/pages/home.php
+++ backend/lang/en/pages/home.php
@@ -1,16 +1,10 @@
 <?php
 return [
-    'hero.line1' => 'Future-focused.',
-    'hero.line2' => 'Innovation fueled.',
-    'hero.line3' => 'Built to ',
-    'hero.line3.strong' => 'disrupt.',
-    'sec2.title' => 'Hello World',
-    'sec2.description' => 'I’m Mason Root, an Eagle Scout and fullstack software engineer with a passion for building innovative solutions. With a strong foundation in both tech and leadership, I\'m constantly pushing boundaries and aspiring to create my own ventures. Let\'s build something extraordinary together!',
-    'sec3.whyme' => 'Why Me?',
-    'sec3.reason1.alt' => 'A lock and shield',
-    'sec3.reason1' => 'Security is my top priority. I build powerful, safe solutions with proactive, cutting-edge protection from day one.',
-    'sec3.reason2.alt' => 'a super fast looking car',
-    'sec3.reason2' => 'Speed is my second priority. I build applications as fast and efficient as a high-performance car, ensuring seamless user experiences.',
-    'sec3.reason3.alt' => 'a cloud computing network',
-    'sec3.reason3' => 'Scalability is my third priority. I design solutions built to grow seamlessly with your business, ensuring long-term performance and reliability.',
+    "hero" => [
+        "h1" => [
+            "sentence" => "Infinite Possibilities ",
+            "highlighted" => "Ahead"
+        ],
+        "h2" => "I shed light on new ideas and opportunities, illuminating the path forward."
+    ]
 ];
