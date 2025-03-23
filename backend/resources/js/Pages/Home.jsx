8bc970b3ab0941dc44f12bea28a2a191d3d55feb
Mason Root <contact@masonroot.dev>
2025-03-23 13:16:41 -0700

    Added inquiry  section to home page

--- backend/resources/js/Pages/Home.jsx
+++ backend/resources/js/Pages/Home.jsx
@@ -1,5 +1,6 @@
 import '@/../css/Pages/Home.scss';
 import Navbar from '@/Components/Navbar.jsx';
+import { useForm } from '@inertiajs/react';
 import { useTranslation } from 'react-i18next';
 
 function ProjectCard({ title, description, image, link, tags }) {
@@ -32,9 +33,73 @@ function ServiceCard({ icon, title, alt, description }) {
     );
 }
 
+function ContactForm({ t }) {
+    const { data, setData, post, errors } = useForm({
+        name: '',
+        email: '',
+        subject: '',
+        message: '',
+    });
+
+    const handleChange = (e) => {
+        setData(e.target.id, e.target.value);
+    };
+
+    const submit = (e) => {
+        e.preventDefault();
+        post(route('contact'), data);
+    };
+
+    if (Object.values(errors).length > 0) {
+        document.getElementById('subject')
+            .scrollIntoView({ behavior: 'smooth' });
+    }
+
+    return (
+        <>
+            <form onSubmit={submit}>
+                <p className={Object.values(errors).length > 0 ? 'show' : ''}>
+                    {Object.values(errors)[0]}
+                </p>
+                <div>
+                    <input
+                        type="text"
+                        id="name"
+                        placeholder={t('name')}
+                        value={data.name}
+                        onChange={handleChange}
+                    />
+                    <input
+                        type="email"
+                        id="email"
+                        placeholder={t('email')}
+                        value={data.email}
+                        onChange={handleChange}
+                    />
+                </div>
+                <input
+                    type="text"
+                    id="subject"
+                    placeholder={t('subject')}
+                    value={data.subject}
+                    onChange={handleChange}
+                />
+
+                <textarea
+                    id="message"
+                    placeholder={t('message')}
+                    value={data.message}
+                    onChange={handleChange}
+                ></textarea>
+
+                <button type="submit">{t('send_message')}</button>
+            </form>
+        </>
+    );
+}
+
 export default function Home({ stats }) {
     const { t } = useTranslation('home');
-
     return (
         <div id="home">
             <div className="section1">
@@ -122,6 +187,11 @@ export default function Home({ stats }) {
                         description={t('section3.service4.description')}
                     />
                 </div>
+            </div>
+
+            <div id="contact">
+                <h2>{t('section4.h2')}</h2>
+                <ContactForm t={t} />
             </div>
         </div>
     );
