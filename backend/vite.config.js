31b4bb8f642ef4450382c631b818a50bec34907d
Mason Root <contact@masonroot.dev>
2025-04-12 15:27:41 -0700

    Configured vite server for only development environment

--- backend/vite.config.js
+++ backend/vite.config.js
@@ -11,13 +11,15 @@ export default defineConfig({
         }),
         react(),
     ],
-    server: {
-        host: '0.0.0.0',
-        port: 5173,
-        strictPort: true,
-        origin: `${process.env.DDEV_PRIMARY_URL.replace(/:\d+$/, '')}:5173`,
-        cors: {
-            origin: /https?:\/\/([A-Za-z0-9\-\.]+)?(\.ddev\.site)(?::\d+)?$/,
+    ...(process.env.NODE_ENV === 'development' && {
+        server: {
+            host: '0.0.0.0',
+            port: 5173,
+            strictPort: true,
+            origin: `${process.env.DDEV_PRIMARY_URL.replace(/:\d+$/, '')}:5173`,
+            cors: {
+                origin: /https?:\/\/([A-Za-z0-9\-\.]+)?(\.ddev\.site)(?::\d+)?$/,
+            },
         },
-    },
+    }),
 });
