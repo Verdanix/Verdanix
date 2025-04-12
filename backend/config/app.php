4811cb0dd07b98e840ee60ab7ce3bac2fd544214
Mason Root <contact@masonroot.dev>
2025-04-12 15:49:48 -0700

    Add Inception date to app config

--- backend/config/app.php
+++ backend/config/app.php
@@ -122,5 +122,5 @@ return [
         'driver' => env('APP_MAINTENANCE_DRIVER', 'file'),
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
-
+    "career_start_year" => env("CAREER_START_YEAR", 2019),
 ];
