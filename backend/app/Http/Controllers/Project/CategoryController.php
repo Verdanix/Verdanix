a85ac912473388a4d578e9b8dc517e4e2d691193
Mason Root <contact@masonroot.dev>
2025-06-01 15:14:44 -0700

    Added endpoint to fetch categories list and Unit tests

---
+++ backend/app/Http/Controllers/Project/CategoryController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Project;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Category::pluck('name') ->toArray();
+    }
+}
