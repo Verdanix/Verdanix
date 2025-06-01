ad525dc286aab3be725734065ae3ec5710ecc759
Mason Root <contact@masonroot.dev>
2025-05-31 17:00:15 -0700

    Updated migrations
    Removed
    - Old work that I have no idea the purpose of
    Added
    - New work that has a purpose

--- backend/database/migrations/2025_05_31_183128_create_projects.php
+++ backend/database/migrations/2025_05_31_183128_create_projects.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,11 +12,15 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('slug')->unique();
             $table->string('title');
-            $table-> string('slug')->unique();
+            $table->string("client-name")->nullable();
+            $table->string("video");
+            $table->string("github")->nullable();
+            $table->string("live")->nullable();
             $table->string("description");
-            $table->string("challenges");
-            $table->string("solutions");
+            $table->json("challenges")->default(json_encode([]));
+            $table->json("solutions")->default(json_encode([]));
             $table->timestamps();
         });
 
