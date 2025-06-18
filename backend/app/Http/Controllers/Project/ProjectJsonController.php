12b6f346abfed94986414075b1d37e4fc121a6d7
Mason Root <contact@masonroot.dev>
2025-06-17 21:09:02 -0700

    Updated ProjectJsonController
    Fixed
    - The tag ids are now mapped to dictionaries

--- backend/app/Http/Controllers/Project/ProjectJsonController.php
+++ backend/app/Http/Controllers/Project/ProjectJsonController.php
@@ -4,25 +4,40 @@ namespace App\Http\Controllers\Project;
 
 use App\Http\Controllers\Controller;
 use App\Models\GalleryProject;
+use App\Models\Tags;
 use Illuminate\Http\Request;
 
 class ProjectJsonController extends Controller
 {
     public function getAll()
     {
-        return GalleryProject::get()->groupBy('type')->toArray();
+        return GalleryProject::get()->map(function ($project) {
+            return $this->convertTags($project);
+        })->groupBy('type')->toArray();
+    }
+
+    function convertTags($project): array
+    {
+        $project->tech_stack = array_map(function ($tag) {
+            return Tags::where('id', $tag)->first();
+        }, json_decode($project->tech_stack));
+
+        return $project->toArray();
     }
 
     public function getCategory(int $category)
     {
-        return GalleryProject::where('category_id', $category)->get()->groupBy('type');
+        return GalleryProject::where('category_id', $category)->get()->map(function ($project) {
+            return $this->convertTags($project);
+        })->groupBy('type');
     }
 
     public function getType(string $type)
     {
-        return GalleryProject::where('type', $type)->get()->groupBy('type');
+        return GalleryProject::where('type', $type)->get()->map(function ($project) {
+            return $this->convertTags($project);
+        })->groupBy('type');
     }
-
 
     public function search(Request $request)
     {
