40cd5519e2297e1449c0e253c7293e4f22b877b3
Mason Root <contact@masonroot.dev>
2026-04-18 17:40:23 -0700

    Added Redis to DDEV and added limiter config option to cache.php

--- config/cache.php
+++ config/cache.php
@@ -17,6 +17,7 @@ return [
 
     'default' => env('CACHE_STORE', 'database'),
 
+    'limiter' => env('CACHE_RATE_LIMITER', 'redis'),
     /*
     |--------------------------------------------------------------------------
     | Cache Stores
