a1b4426566ad1ae3ca15206e42c981d95ac012cd
Mason Root <contact@masonroot.dev>
2026-04-21 22:00:21 -0700

    Added SEO for Contact page

---
+++ lang/en/seo/contact.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'meta_title' => 'Contact',
+    'meta_description' => 'Inquire about high-stakes systems architecture, industrial automation, and specialized full-stack development. Initiate a handshake for technical consulting.',
+    'meta_keywords' => 'Systems Architecture, Industrial Automation, Physical Computing, Embedded Systems, Linux Ops, Docker Orchestration, Backend Scaling, Technical Consultant',
+];
