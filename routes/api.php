6731a7eb0b5c08ab2954e9c468bca1a81f977927
Mason Root <contact@masonroot.dev>
2026-04-21 22:02:12 -0700

    Added Contact page and POST inquiry endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\InquiryController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/contact', [InquiryController::class, 'store'])->name('submit.inquiry')->middleware(['throttle:contact-inquiry']);
+Route::fallback(fn() => response()->json([], 404));
