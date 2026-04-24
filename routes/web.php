b51e93593a579a1804213e322c3067be99c71749
Mason Root <contact@masonroot.dev>
2026-04-23 18:02:23 -0700

    Removed Unused Code

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,8 @@
 <?php
 
 use App\Http\Controllers\InquiryController;
-use App\Http\Controllers\ProfileController;
 use App\Services\PageRenderingService;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 Route::get('/', [PageRenderingService::class, 'renderPage'])->setDefaults(['page' => 'Landing'])->name('landing');
 Route::get('/about', [PageRenderingService::class, 'renderPage'])->setDefaults(['page' => 'About'])->name('about');
@@ -13,25 +10,5 @@ Route::get('/projects', [PageRenderingService::class, 'renderPage'])->setDefault
 Route::get('/hobbies/motorcycling', [PageRenderingService::class, 'renderPage'])->setDefaults(['page' => 'About'])->name('hobbies.motorcycling');
 Route::get('/contact', [PageRenderingService::class, 'renderPage'])->setDefaults(['page' => 'Contact'])->name('contact');
 Route::post('/contact', [InquiryController::class, 'store'])->name('submit.inquiry')->middleware(['throttle:contact-inquiry']);
-
-/*
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
-
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});*/
 
 require __DIR__.'/auth.php';
