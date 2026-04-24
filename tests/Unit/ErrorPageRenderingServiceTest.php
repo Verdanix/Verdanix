6a055052ed38b5e3a340d5f6bf7b133590d4b6bf
Mason Root <contact@masonroot.dev>
2026-04-23 19:13:00 -0700

    Moved test cases to proper folder

--- tests/Unit/ErrorPageRenderingServiceTest.php
+++ tests/Unit/ErrorPageRenderingServiceTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Unit;
 
 use App\Services\ErrorPageRenderingService;
 use Tests\TestCase;
--- tests/Unit/InquiryRequestTest.php
+++ tests/Unit/InquiryRequestTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Unit;
 
 use App\Http\Requests\InquiryRequest;
 use Tests\TestCase;
